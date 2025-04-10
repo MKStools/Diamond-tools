4d5f742f8bbe140bce8efc8b0037f2751f3ea0ba
MKStools <manojs965@gmail.com>
2025-04-10 10:26:51 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to MKS Diamond Tools Consulting</h1>
+      <p>This website is under construction. Get ready to experience cutting-edge innovation.</p>
+    </div>
+  );
+}
